37d20163434554e726dae4fd69a76f8d7c641dc4
NayaraMenezes <Nayaramenezes.dev@gmail.com>
2022-08-13 23:47:55 -0300

    Entrega do desafio Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BookstoreComponent } from './components/bookstore/bookstore.component';
+import { FiltersComponent } from './components/bookstore/filters/filters.component';
+import { ProductListComponent } from './components/bookstore/product-list/product-list.component';
+import { ProductItemComponent } from './components/bookstore/product-list/product-item/product-item.component';
+import { ProductListService } from './components/bookstore/product-list/product-list.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    BookstoreComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ProductListService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
